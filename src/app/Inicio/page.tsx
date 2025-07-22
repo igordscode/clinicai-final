dff8a9fa74271c25bf33f3a23f544f5ca2311c0b
igordscode <igordsmarketing@gmail.com>
2025-07-21 21:00:04 -0300

    build: Placeholder in Home Page

--- src/app/Inicio/page.tsx
+++ src/app/Inicio/page.tsx
@@ -1,1 +1,11 @@
-w
+import React from 'react';
+
+// Esqueleto básico para a página de Dashboard
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Esta é a página principal do Dashboard.</p>
+    </div>
+  );
+}
